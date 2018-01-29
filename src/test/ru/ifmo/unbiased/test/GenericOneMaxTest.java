3e64972e0cd0554e77b3a48e6c230ea20db5f429
Maxim Buzdalov <mbuzdalov@gmail.com>
2018-01-30 01:38:04 +0300

    Add some runtime tests for the current version of the generic algo.

--- src/test/ru/ifmo/unbiased/test/GenericOneMaxTest.java
+++ src/test/ru/ifmo/unbiased/test/GenericOneMaxTest.java
@@ -1,5 +1,6 @@
 package ru.ifmo.unbiased.test;
 
+import org.junit.Assert;
 import org.junit.Test;
 import ru.ifmo.unbiased.UnbiasedProcessor;
 import ru.ifmo.unbiased.algo.GenericOneMax;
@@ -13,15 +14,76 @@ public class GenericOneMaxTest {
             for (int n = 1; n <= (arity == 6 ? 20 : 100); ++n) {
                 int count = 10;
                 UnbiasedProcessor processor = new UnbiasedProcessor(n, arity, ImmutableBitArray::cardinality, n);
-                int sum = 0;
                 for (int i = 0; i < count; ++i) {
-                    sum += GenericOneMax.runGeneric(processor);
+                    GenericOneMax.runGeneric(processor);
                 }
-
-                double avg = (double) (sum) / count;
-
-                System.out.println("OneMax::generic(n = " + n + ", arity = " + arity + "): average = " + avg);
             }
+        }
+    }
+
+    @Test
+    public void runtimeGeneric3() {
+        int n = 239;
+        int count = 100;
+        UnbiasedProcessor processor = new UnbiasedProcessor(n, 3, ImmutableBitArray::cardinality, n);
+        int sum = 0;
+        for (int i = 0; i < count; ++i) {
+            sum += GenericOneMax.runGeneric(processor);
+        }
+
+        double avg = (double) (sum) / count;
+        double expected = 2.0 * n;
+        System.out.println("GenericOneMax, arity 3: average = " + avg + " = (2 n) * " + (avg / expected));
+
+        if (sum > expected * count * 1.01) {
+            Assert.fail("GREATER: n is " + n + ", sum is " + sum + ", average is " + avg);
+        }
+        if (sum < expected * count * 0.99) {
+            Assert.fail("LESS: n is " + n + ", sum is " + sum + ", average is " + avg);
+        }
+    }
+
+    @Test
+    public void runtimeGeneric4() {
+        int n = 239;
+        int count = 100;
+        UnbiasedProcessor processor = new UnbiasedProcessor(n, 4, ImmutableBitArray::cardinality, n);
+        int sum = 0;
+        for (int i = 0; i < count; ++i) {
+            sum += GenericOneMax.runGeneric(processor);
+        }
+
+        double avg = (double) (sum) / count;
+        double expected = 37.0 * n / 34.0;
+        System.out.println("GenericOneMax, arity 4: average = " + avg + " = (37/34 n) * " + (avg / expected));
+
+        if (sum > expected * count * 1.01) {
+            Assert.fail("GREATER: n is " + n + ", sum is " + sum + ", average is " + avg);
+        }
+        if (sum < expected * count * 0.99) {
+            Assert.fail("LESS: n is " + n + ", sum is " + sum + ", average is " + avg);
+        }
+    }
+
+    @Test
+    public void runtimeGeneric5() {
+        int n = 239;
+        int count = 100;
+        UnbiasedProcessor processor = new UnbiasedProcessor(n, 5, ImmutableBitArray::cardinality, n);
+        int sum = 0;
+        for (int i = 0; i < count; ++i) {
+            sum += GenericOneMax.runGeneric(processor);
+        }
+
+        double avg = (double) (sum) / count;
+        double expected = 23.0 * n / 32.0;
+        System.out.println("GenericOneMax, arity 5: average = " + avg + " = (23/32 n) * " + (avg / expected));
+
+        if (sum > expected * count * 1.01) {
+            Assert.fail("GREATER: n is " + n + ", sum is " + sum + ", average is " + avg);
+        }
+        if (sum < expected * count * 0.99) {
+            Assert.fail("LESS: n is " + n + ", sum is " + sum + ", average is " + avg);
         }
     }
 }
